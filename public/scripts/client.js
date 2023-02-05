c05c899b52200a6f07fb35185f9fde102d2689ea
Sicily Ding <xishading@gmail.com>
2023-02-05 13:02:32 -0700

    display errors

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,20 +3,26 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
 const createTweetElement = function(tweetObj) {
+  const user = tweetObj.user;
   const $tweet = $(`
   <article class="tweet-container">
         <header>
           <div class="user-info">
-          <div><img src="${tweetObj.user.avatars}" alt="user-pic"></div>
-          <span>DizzySloth</span>
+          <div><img src="${user.avatars}" alt="user-pic"></div>
+          <span>${user.name}</span>
           </div>
-          <span class="user-handle">${tweetObj.user.handle}</span>
+          <span class="user-handle">${user.handle}</span>
         </header>
-        <div class="tweet-content">${tweetObj.content.text}</div>
+        <div class="tweet-content">${escape(tweetObj.content.text)}</div>
         <footer>
-          <div class="timeago" datetime="2016-06-30 09:20:00>${tweetObj.created_at}</div>
+          <div class="timeago">${timeago.format(tweetObj.created_at)}</div>
           <div>
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -30,29 +36,9 @@ const createTweetElement = function(tweetObj) {
 
 
 const renderTweets = function(tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-    for (tweet of tweets) {
-      const tweetElement = createTweetElement(tweet);
-      $(".tweets-container").append(tweetElement);
-    }
-    return;
-};
-
-const postTweets = function () {
-    $.ajax({
-        url: "/tweets",
-        method: "POST",
-        data: $(".tweet-form").serialize(),
-        dataType: "json",
-        success: (data) => {
-            console.log(data);
-        },
-        error: (error) => {
-            console.error(error);
-        }
-    });
+  for (const tweet of tweets) {
+    $(".tweets-container").append(createTweetElement(tweet));
+  }
 };
 
 const loadTweets = function () {
@@ -61,19 +47,34 @@ const loadTweets = function () {
         method: "GET",
         dataType: "json",
         success: (data) => {
-            console.log(data);
+          renderTweets(data);
         },
         error: (error) => {
-            console.error(error);
+          console.error(error);
         }
     });
 };
 
 $(document).ready(function () {
-    renderTweets(data);
+    $(".alert").css("display", "none");
+    
     loadTweets();
-  $(".tweet-form").on("submit", function(event) {
+
+    $(".tweet-form").on("submit", function(event) {
     event.preventDefault();
-    postTweets();
-  }) 
+
+    const tweet = $("#tweet-text").val();
+    if (!tweet) {
+      return $('.alert').html('<i class="fa-solid fa-triangle-exclamation"></i> Empty tweet!').slideDown();
+    }
+    if (tweet.length > 140) {
+      return $('.alert').html('<i class="fa-solid fa-triangle-exclamation"></i> Too long! Plz rspct our arbitrary limit of 140 chars.<i class="fa-solid fa-triangle-exclamation"></i> ').slideDown();
+    }
+    const newTweetData = $(this).serialize();
+    $('#tweet-text').val('');
+    $(this.counter).val(140);
+    $('.alert').text('');
+    $('.alert').slideUp('slow');
+    $.post("/tweets/", newTweetData, loadTweets)
+  });
 });
